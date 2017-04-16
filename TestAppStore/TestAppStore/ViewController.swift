4e622b4341604b5926fa8073308f0869e1cdc435
Moonbeom Kyle KWON <maskkwon@gmail.com>
2017-04-16 21:40:29 +0900

    init project

---
+++ TestAppStore/TestAppStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppStore
+//
+//  Created by Moonbeom Kyle KWON on 2017. 4. 16..
+//  Copyright © 2017년 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
